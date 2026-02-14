96d1cf647ac81a9c33fa63c7928743d4d64a4340
Rohit <r@gmail.com>
2026-02-14 21:13:29 +0530

    Commit successfuly maven project

---
+++ src/main/java/com/example/rohit/App.java
@@ -0,0 +1,13 @@
+package com.example.rohit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello, I am Rohit Kumar!" );
+    }
+}
